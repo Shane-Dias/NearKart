c068fdc02d8727b593a314a46b4ef6da7a9d57aa
Shane-Dias <160272459+Shane-Dias@users.noreply.github.com>
2025-05-26 23:22:41 +0530

    db setup

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ DB connexted:", conn.connection.host);
+  } catch (error) {
+    console.error("❌ DB connection failed:", error.message);
+  }
+};
+
+export default connectDB;
